1938172472240d3ce6271e32d41ff02d7b1c936d
vedha98 <vetha.gnanam98@gmail.com>
2018-01-03 22:58:18 -0800

    change
    changed user name and pass

--- config/database.js
+++ config/database.js
@@ -1,2 +1,2 @@
-module.exports = {database:'mongodb://guest:guest123@ds133627.mlab.com:33627/todoapp',
+module.exports = {database:'mongodb://admin:admin@ds133627.mlab.com:33627/todoapp',
 secret:'secrets'};
